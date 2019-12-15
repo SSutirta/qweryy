d9ee2ffbb648ffd2ad8efcde3d06468f1ae9d75c
Steven Sutirta <noonelonglostson@gmail.com>
2019-12-15 16:53:37 +0700

    fasttext done

--- ft.py
+++ ft.py
@@ -2,16 +2,18 @@ import os
 import io
 import fasttext
 
-files = []
-file1 = io.open("text.txt", "w", encoding="utf-8")
-for r, d, f in os.walk('D:'):
-    for file in f:
-        file1.write(file+"\n")
+#files = []
+#file1 = io.open(r"C:\Users\AlnthraevaN\Documents\Skripsi\text.txt", "w", encoding="utf-8")
+#for r, d, f in os.walk('D:'):
+#    for file in f:
+#        file1.write(r + '\\' + file + "\n")
 		#files.append( file + "\n")
 #for f in files:
 	#file1.writelines(f)
-file1.close()
-model = fasttext.train_unsupervised("text.txt", model='skipgram')
+#file1.close()
 
-model.get_nearest_neighbors('ana', k=100)
-print(model['ana'])
+
+model = fasttext.load_model(r"C:\Users\AlnthraevaN\Documents\Skripsi\cc.en.300.bin")
+
+model.get_nearest_neighbors('dog', k=100)
+#print(model['ana'])
---
+++ openfolder.py
@@ -0,0 +1,5 @@
+import os
+
+path="D:Battle.net\\"
+path=os.path.realpath(path)
+os.startfile(path)
